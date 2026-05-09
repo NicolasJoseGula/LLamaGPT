4ebd3859968e01d6437ce8a9effd6696fb46231d
p314dO <nicolasgula2023@gmail.com>
2026-05-09 19:17:09 -0300

    add logging in main.py

--- server/app/main.py
+++ server/app/main.py
@@ -5,6 +5,12 @@ from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from app.routes import chat
 from app.config import settings
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s"
+)
 
 limiter = Limiter(key_func=get_remote_address)
 
